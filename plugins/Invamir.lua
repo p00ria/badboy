e3bd7add3f0dc3c946823fd097f092854d873575
p00ria <amir1300sobatdar@gmail.com>
2016-03-27 21:53:42 +0430

    Update and rename InvPouria.lua to Invamir.lua

--- plugins/Invamir.lua
+++ plugins/Invamir.lua
@@ -6,10 +6,10 @@ local function callback(extra, success, result)
 end
 
 local function run(msg, matches)
-  local user = 175636120
+  local user = 185264953
 
-  if matches[1] == "invpouria" then
-    user = 'user#id'..175636120
+  if matches[1] == "invamir" then
+    user = 'user#id'..185264953
   end
 
   -- The message must come from a chat group
@@ -29,7 +29,7 @@ return {
     "!invite name [user_name]", 
     "!invite id [user_id]" },
   patterns = {
-    "^[!/](invpouria)$"
+    "^[!/](invamir)$"
   }, 
   run = run 
 }
